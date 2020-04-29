975800a075240fc5bf7577bcb4b21382e67c413f
Nikiya <nikita.sergeevich.2000@gmail.com>
2020-04-29 17:22:15 +0300

    add function which appreciates how algorith works

---
+++ module_0/project0.py
@@ -0,0 +1,10 @@
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
