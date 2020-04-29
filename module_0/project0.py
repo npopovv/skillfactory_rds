3ccda577bb8158ad1af2f21f498519f617af612f
Nikiya <nikita.sergeevich.2000@gmail.com>
2020-04-29 17:32:40 +0300

    add my 2nd algoritm v4

--- module_0/project0.py
+++ module_0/project0.py
@@ -11,3 +11,22 @@ def game_core_v3(number):
         predict = np.random.randint(a,b)
         count+=1
     return count
+   from math import ceil
+def game_core_v4(number):
+    bottom = 0
+    top = 100
+    count = 1
+    while bottom != top:
+        sep = number - bottom
+        half = ceil((top-bottom)/2)
+        if sep > half:
+            bottom += half
+        elif sep < half:
+            top -= half
+        elif sep == half:
+            bottom = bottom + sep
+    predict = bottom
+    while predict != number:
+        predict += 1
+        count += 1
+    return count
