2c0491b24c5cb764e23255ad1b672ba7b18c0b18
Nikiya <nikita.sergeevich.2000@gmail.com>
2020-04-29 19:21:44 +0300

    add final version 5

--- module_0/project0.py
+++ module_0/project0.py
@@ -30,3 +30,39 @@ def game_core_v4(number):
         predict += 1
         count += 1
     return count
+from math import floor
+def game_core_v5(number):
+    predict = np.random.randint(1,101)
+    count = 1
+    a=1
+    b=101
+    while number != predict:
+        if number > predict:
+            a = predict
+            middle = a + ((b-a)/2)
+            #if number == ceil(middle):   С этим if почему-то неправильно угадывается число  
+            #    pridict = ceil(middle)
+            #    break
+            if number == int(middle):
+                predict = int(middle)
+                break
+            if number > middle:
+                a = floor(middle) + 1 
+            elif number < middle:
+                b = ceil(middle)
+        else:
+            b = predict
+            middle = a + ((b-a)/2)
+            #if number == ceil(middle):     С этим if почему-то неправильно угадывается число  
+            #    pridict = ceil(middle)
+            #    break
+            if number == int(middle):
+                predict = int(middle)
+                break
+            if number < middle:
+                b = ceil(middle)
+            elif number > middle:
+                a = floor(middle) + 1 
+        predict = np.random.randint(a,b)
+        count+=1
+    return count
